46e59b6650b7d6b82acea78cb994827ec6db2678
SOFIANYY <sofian.yahyaoui4@gmail.com>
2022-09-25 02:33:36 +0200

    Ex 4: create HomePage component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <h1>Cleanup reussi</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HomePage msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
+  components: {
+    HomePage
+  }
 }
 </script>
 
